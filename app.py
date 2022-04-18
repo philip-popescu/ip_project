b64c9da543cf88c6ee90e654bd2a804dc3f7d314
philip-popescu <popescu_philip@yahoo.ro>
2022-04-18 17:50:48 +0300

    SAVED

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
+def check_login_type(login_type: str) -> bool:
+    return login_type in ['employee', '']
+
+
 @app.route('/')
 def main():
     return render_template('index.html')
@@ -10,4 +14,11 @@ def main():
 
 @app.route("/login/<string:login_type>")
 def login(login_type):
-    return redirect("/")
+    if check_login_type(login_type):
+        return redirect("/")
+
+
+@app.route("/user/<string:login_type>")
+def user(login_type):
+    if check_login_type(login_type):
+        return render_template("SignedIn.html", login_type=login_type)

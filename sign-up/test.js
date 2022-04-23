c8d60075257e04dd1f8feafaa351baab2de340eb
radu-andrei-oprea <61631356+radu-andrei-oprea@users.noreply.github.com>
2022-04-23 14:48:08 +0300

    added sign-up demo pages
    basic sign-up page.
    test.html - sign-up page
    welcome.html - welcoming page
    "telefon" checks for the format [07][0-9]{8}.
    "email" checks for standard email format.

---
+++ sign-up/test.js
@@ -0,0 +1,73 @@
+function showPass() {
+    const x = document.getElementById("passw");
+    if (x.type === "password") {
+        x.type = "text";
+    }
+    else {
+        x.type = "password";
+    }
+}
+
+function displayErr(check) {
+
+    let err = document.getElementById("alert");
+    let new_message = document.getElementById("message")
+
+    if (check == 0) {
+        let n = sessionStorage.setItem(0, document.getElementById("nume").value)
+        location.href = "welcome.html";
+    }
+    else if (check > 1) {
+        err.style.opacity = 1;
+        err.innerHTML = "Parolele nu sunt identice!";
+        document.getElementById("passw").value = '';
+        document.getElementById("r_passw").value = '';
+    }
+    else{
+        err.style.opacity = 1;
+        err.innerHTML = "Date incorecte introduse!"
+        document.getElementById("nume").value = '';
+        document.getElementById("telefon").value = '';
+        document.getElementById("email").value = '';
+        document.getElementById("passw").value = '';
+        document.getElementById("r_passw").value = '';
+    }
+    setTimeout(function () { err.style.opacity = 0;}, 1000);
+
+}
+
+function checkCredentials() {
+    let ret = 0;
+
+    if (document.getElementById("nume").value === "") {
+        ret = 1;
+        console.log("name null")
+    }
+
+    let p = document.getElementById("passw");
+    if (p.length < 8) {
+        ret = 1;
+        console.log("password too short");
+    }
+
+    let t = document.getElementById("telefon");
+    let e = document.getElementById("email");
+    let t_pattern = /[07]{2}[0-9]{2}[0-9]{3}[0-9]{3}/;
+    let e_pattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+
+    if ( !RegExp(t_pattern).test(t.value) || !RegExp(e_pattern).test(e.value)){
+        ret = 1;
+        console.log("phone match: " + t.value.match(t_pattern));
+        console.log("mail match: " + e.value.match((e_pattern)));
+
+    }
+
+    let rp = document.getElementById("r_passw");
+    if (p.textContent !== rp.textContent){
+        ret = 2;
+        console.log("different passwords");
+    }
+    console.log(ret);
+
+    displayErr(ret);
+}

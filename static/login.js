e5b7e7bb76210fa544b2435a4fcf1f4b69396f05
radu-andrei-oprea <ouser99@icloud.com>
2022-04-30 11:35:20 +0300

    -added image.html -updated 2FactorId.html -updated login.css, Login.html, login.css

--- static/login.js
+++ static/login.js
@@ -8,14 +8,47 @@ function showPass() {
     }
 }
 
-function displayErr() {
+function displayErr(alert_id) {
 
-    console.log("show err");
-    let err = document.getElementById("alert");
-
-    err.style.opacityty = 1;
-    document.getElementById("email").value = '';
-    document.getElementById("passw").value = '';
-
+    let err = document.getElementById(alert_id);
+    err.style.opacity = 1;
     setTimeout(function () { err.style.opacity = 0;}, 1000);
+}
+
+function verify_Login() {
+
+    let ret = 0;
+    let p = document.getElementById("passw");
+    if (p.length < 8) {
+        ret = 1;
+        console.log("password too short");
+    }
+
+    let e = document.getElementById("email");
+    let e_pattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+
+    if (!RegExp(e_pattern).test(e.value)){
+        ret = 1;
+        console.log("mail match: " + e.value.match((e_pattern)));
+    }
+
+    if (ret == 1) {
+        displayErr("alertLogin");
+    }
+    else {
+        document.getElementById("submitForm").submit();
+    }
+}
+
+function verify_2fa() {
+
+    let code = document.getElementById("2fa");
+
+    if (code.value == "") {
+        console.log("empty")
+        displayErr("alert2FA");
+    } else {
+        console.log(code.value)
+        document.getElementById("2FA_form").submit();
+    }
 }

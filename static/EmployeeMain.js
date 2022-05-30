6633c72bcb6e6e63b9174c20391544da4cc8997f
radu-andrei-oprea <ouser99@icloud.com>
2022-05-30 18:56:49 +0300

    -code for checkboxes

--- static/EmployeeMain.js
+++ static/EmployeeMain.js
@@ -6,12 +6,19 @@ function roomsAvailable() {
     let rez = {{rezervari}};
     let hotel_data = {{hotel_data}};
 
-    const ids = [];
+    const no_ids = [];
+    const yes_ids = [];
 
     for (let i in rez){
         if ( (from >= i.from && to <= i.to)
         || (to >= i.from && to <= i.to))
-            ids.push(i.id);
+            no_ids.push(i.room_id);
     }
 
+    for (let j in hotel_data) {
+        if( !no_ids.includes(j.rooms[0]))
+            yes_ids.push(j.rooms);
+    }
+
+    return yes_ids;
 }
